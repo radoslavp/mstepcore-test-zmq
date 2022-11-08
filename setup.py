ad1cfde904005f6db2727992d5d805075459dabc
Radoslav Pesek <radoslav.pesek@microstep-mis.com>
2022-11-08 12:35:58 +0100

    Added setup.py so it is buildable by yocoto's bitbake/devtool.

---
+++ setup.py
@@ -0,0 +1,13 @@
+import sys
+from setuptools import setup
+setup(
+    name = "core-test-zmq",
+    version = "0.1",
+    packages=["."],
+    author="Radoslav Pesek",
+    author_email = "radoslav.pesek@microstep-mis.com",
+    description = "Core app & modules using zeromq library",
+    license = "proprietary",
+    keywords= "zeromq",
+    url = "",
+)
