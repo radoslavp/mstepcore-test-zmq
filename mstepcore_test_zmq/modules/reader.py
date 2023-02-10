4edd5f99f92a1359a421eafa03bf80751f208f17
Kas User <kas@example.com>
2023-02-10 11:41:13 +0100

    Removed mistakenly added spidev code from reader.

--- mstepcore_test_zmq/modules/reader.py
+++ mstepcore_test_zmq/modules/reader.py
@@ -4,11 +4,6 @@ from ..mstep_lib import MstepLib
 
 def module_run(context):
     mstep = MstepLib(context)
-    
-    spi = spidev.SpiDev()
-    spi.open(bus, device)
-    to_send = [0x01, 0x02, 0x03]
-    #spi.xfer(to_send)
 
     variable_label = {  "temp": "temperature",
                         "hum": "humidity",
