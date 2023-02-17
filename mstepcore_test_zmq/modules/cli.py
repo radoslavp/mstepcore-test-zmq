61fe26bc15a916a18e0c40572d9dc902e8326619
Kas User <kas@example.com>
2023-02-17 15:39:48 +0100

    Fixed exception thrown when /dev/ttyGS0 does not exist.

--- mstepcore_test_zmq/modules/cli.py
+++ mstepcore_test_zmq/modules/cli.py
@@ -2,35 +2,40 @@ import serial
 import re
 from ..mstep_lib import MstepLib
 
-ser = serial.Serial('/dev/ttyGS0') 
+ser = None
 
 def ser_write(msg):
     ser.write(msg + b'\r\n')
 
 def module_run(context):
-    mstep = MstepLib(context)
-    buff = b''
-    
-    ser.write(b'> ')
-    while True:
-        c = ser.read(1)
-
-        if c == b'\r':
-            ser.write(b'\r\n')
-        
-            if buff == b'help':
-                ser_write(b'Mstep CLI help:')
-                ser_write(b'\tdevid - prints device id')
-            elif buff == b'devid':
-                ser_write(b'devid = 1')
-            elif re.match(b'get', buff):
-                var = buff.split(b' ')[1].decode("utf-8")
-                val = bytes(mstep.getVar(var), "utf-8")
-                ser_write(val)
-
+    try:
+        with serial.Serial('/dev/ttyGS0') as ser: 
+            mstep = MstepLib(context)
             buff = b''
+    
             ser.write(b'> ')
+            while True:
+                c = ser.read(1)
+
+                if c == b'\r':
+                    ser.write(b'\r\n')
+        
+                    if buff == b'help':
+                        ser_write(b'Mstep CLI help:')
+                        ser_write(b'\tdevid - prints device id')
+                    elif buff == b'devid':
+                        ser_write(b'devid = 1')
+                    elif re.match(b'get', buff):
+                        var = buff.split(b' ')[1].decode("utf-8")
+                        val = bytes(mstep.getVar(var), "utf-8")
+                        ser_write(val)
+
+                        buff = b''
+                    ser.write(b'> ')
                 
-        else:
-            ser.write(c)
-            buff = buff + c
+                else:
+                    ser.write(c)
+                    buff = buff + c
+
+    except serial.serialutil.SerialException as e:
+        print(e)
